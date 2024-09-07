24788dab303db1760fedf1cc98b5db626e2d36bb
Yusra272 <yusraahsankhan1@gmail.com>
2024-09-07 07:45:57 -0700

    assignment3

---
+++ main.dart
@@ -0,0 +1,72 @@
+void main(){
+
+//  Question1
+
+// List names = ['yusra, umna, ayesha, nimra'];
+// print(names);
+
+// Question2
+
+//  List days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+//  print(days[6]);
+
+    // Question3
+
+// List<dynamic> info = ['yusra', '10th', '444921', 'B', 76.85];
+// print('name: ${info[0]}');
+// print('class: ${info[1]}');
+// print('roll no: ${info[2]}');
+// print('grade: ${info[3]}');
+// print('percentage: ${info[4]}');
+
+
+// Question4
+
+  // List<int> numbers = [10, 1, 20, 2, 30, 3];
+
+  // int smallest = numbers.reduce((a, b) => a < b ? a : b);
+  // int greatest = numbers.reduce((a, b) => a > b ? a : b);
+
+  // print('Smallest number: $smallest');
+  // print('Greatest number: $greatest');
+
+  // Question5
+
+  // List<int> numbers = [10, 20, 30, 40, 50];
+  // int maxValue = numbers.reduce((a, b) => a > b ? a : b);
+
+  // print('the Maximum value in the list is: $maxValue');
+
+  
+// Question6
+
+// List string = ['pink', 'purple', 'blue', 'red'];
+// List stringreversed = List.from (string.reversed);
+
+// print('original list: $string' );
+// print('reversed list: $stringreversed');
+
+
+// Question7
+
+  // List<int> numbers = [4, 5, -6, 7, -8, -9];
+
+  // numbers.removeWhere((int) => int < 0);
+
+  // print(numbers);
+
+
+  // Question8
+
+//   List<String> usersEligibility = ['John', 'Alice', 'eligible', 'Mike', 'Sarah', 'Tom'];
+
+//   usersEligibility.removeWhere((user) => user == 'eligible');
+
+//   print('list: usersEligibility');
+
+}
+
+
+ 
+
+
